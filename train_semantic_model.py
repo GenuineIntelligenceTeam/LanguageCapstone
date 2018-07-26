78e11640929a50de66eb8c13f2068840a072fc3a
Ian Coolidge <ian.coolidge@asdnh.org>
2018-07-26 16:24:18 -0400

    added preprocessing for NN training data

--- train_semantic_model.py
+++ train_semantic_model.py
@@ -5,42 +5,55 @@ from mynn.optimizers.sgd import SGD
 import numpy as np
 import mygrad as mg
 
-def train(training_triples, margin=0.5):
+def process_triples(training_triples, caption_embeddings, resnet_embeddings):
     caption_ids, good_img_ids, bad_img_ids = zip(*training_triples)
+    N = len(caption_ids)
 
-    caption_ids = np.array(caption_ids)
-    good_img_ids = np.array(good_img_ids)
-    bad_img_ids = np.array(bad_img_ids)
-    print(good_img_ids.shape)
+    caption_desc = np.zeros((N, 50))
+    for i, caption_id in enumerate(caption_ids):
+        caption_desc[i,:] = caption_embeddings[caption_id]
+
+    good_desc = np.zeros((N, 512))
+    for i, img_id in enumerate(good_img_ids):
+        good_desc[i,:] = resnet_embeddings[img_id]
+    
+    bad_desc = np.zeros((N, 512))
+    for i, img_id in enumerate(bad_img_ids):
+        bad_desc[i,:] = resnet_embeddings[img_id]
+
+    return caption_desc, good_desc, bad_desc
+
+
+def train(training_triples, margin=0.5):
 
     model = SemanticEmbeddingModel()
     optim = SGD(model.parameters, learning_rate=0.1)
 
     resnet_embeddings = load_resnet_embeddings()
 
+    caption_desc, good_desc, bad_desc = process_triples(training_triples, caption_embeddings, resnet_embeddings)
+
     for epoch_cnt in range(5):
-        idxs = np.arange(len(caption_ids))
+        idxs = np.arange(len(caption_desc))
         np.random.shuffle(idxs)  
 
-        caption_ids = caption_ids[idxs]
-        good_img_ids = good_img_ids[idxs]
-        bad_img_ids = bad_img_ids[idxs]
+        caption_desc = caption_desc[idxs]
+        good_desc = good_desc[idxs]
+        bad_desc = bad_desc[idxs]
 
-        for caption_id, good_id, bad_id in zip(caption_ids, good_img_ids, bad_img_ids):
+        for i in range(len(caption_desc)):
 
             batch_caption = np.zeros(50)
-            batch_good_img = resnet_embeddings[good_id]
-            batch_bad_img = resnet_embeddings[bad_id]
+            batch_good_img = good_desc[i,:]
+            batch_bad_img = bad_desc[i,:]
 
             y_pred_good = mg.matmul(batch_caption, model(batch_good_img))
             y_pred_bad =  mg.matmul(batch_caption, model(batch_bad_img))
 
             loss = margin_ranking_loss(y_pred_good, y_pred_bad, 1, margin=margin)
-            # back-propagate through your computational graph through your loss
+
             loss.backward()
-            # execute gradient descent by calling step() of `optim`
             optim.step()
-            # null your gradients (please!)
             loss.null_gradients()
 
-train([(0,0,0),(0,0,0)])
+resnet_embeddings = load_resnet_embeddings()
