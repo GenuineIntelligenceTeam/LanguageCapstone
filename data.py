3d40c555771a55a0fb94380fd64bef5bc9fde7dd
Ashley Teng <ashlaeteng@dhcp-18-111-26-35.dyn.mit.edu>
2018-07-26 15:54:31 -0400

    added getCaptions function

--- data.py
+++ data.py
@@ -27,3 +27,28 @@ def displayImages(image_id):
             f.write(url.read())
     img = Image.open('display.jpg')
     img.show()
+
+def getCaptions(image_id):
+    """ provides all 5 captions for image associated with the given ID
+
+
+    Parameters
+    ----------
+    image_id : int
+        the ID associated with the image that will be displayed
+
+    Returns
+    -------
+    final_captions : list
+        list of all 5 captions of image
+    """
+    image_ids, ids, captions = [], [], []
+    for i in range(len(data['annotations'])):
+        image_ids.append((data['annotations'][i])['image_id'])
+        ids.append((data['annotations'][i])['id'])
+        captions.append((data['annotations'][i])['caption'])
+    caption_dict = dict(zip(ids, captions))
+    img_indices = [i for i, x in enumerate(image_ids) if x == image_id]
+    final_ids = [ids[j] for j in img_indices]
+    final_captions = [caption_dict[k] for k in final_ids]
+    return final_captions
